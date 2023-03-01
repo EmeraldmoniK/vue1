155cdcd09acd5e51e3ed5e59acd5849ee26ec1c1
LAZA <34@ry.com>
2023-03-01 14:58:28 +0500

    DSDS

---
+++ main.js
@@ -0,0 +1,18 @@
+const app = {
+    data(){
+        return {
+            text:'',
+            background: '',
+            red: 'red',
+            green: 'green',
+            blue: 'blue',
+
+
+        }
+    },
+    methods: {
+
+    }
+}
+
+Vue.createApp(app).mount('#app')
